b6890b7e08b1ada4b9323cab422b35f0dbab7980
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-28 21:41:18 +0300

    created the navbar and styled it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
-import Pages from "./pages/Pages";
-
+import { Routes,Route,} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello</h1>
-  <Pages />
-        </div>
+    <div>
+      
+      <NavBar />
+      <Routes>
+        <Route  path="/about" element={<About/>}/>
+        <Route  path="/login" element={<Login/>}/>
+        <Route  path="/" element={<Home/>}/>
+      </Routes>
+    
+    </div>
   );
 }
 
-export default App
+export default App;
