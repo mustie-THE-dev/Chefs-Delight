20a15125e998f55e7f7e4985af09226bee5dd9ca
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-28 16:19:24 +0300

    made changes to json file

--- src/components/Exciting.jsx
+++ src/components/Exciting.jsx
@@ -5,26 +5,16 @@ function Exciting() {
   const [Exciting, setExciting] = useState([]);
 
   useEffect(() => {
-    getExciting();
+   fetch("http://localhost:8000/recipes")
+   .then((res)=> {
+     res.json()
+
+   })
+   .then((data)=> {
+  setExciting(data)
+  console.log(Exciting);
+   })
   }, []);
-
-  // fetching data from a public API
-  const APP_ID = "112046c8";
-  const APP_KEY = "1f296129c2a0d3314528cee815e5d631";
- // const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-  const getExciting = async () => {
-    const api = await fetch(`
- https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
-
-    const data = await api.json();
-    setExciting(data.recipes)
-  };
-
-  return <div>
-
-    {Exciting.map}
-  </div>;
 }
-
-export default Exciting;
+  
+export default Exciting
