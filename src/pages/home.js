377691d1a7eb57268e0c4082572a37c38f9fd6e6
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-28 18:16:46 +0300

    created home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SignUp = () => {
+const Home = () => {
   return (
     <div
       style={{
@@ -10,9 +10,9 @@ const SignUp = () => {
         height: '90vh'
       }}
     >
-      <h1>Sign Up</h1>
+      <h1>Welcome to Chefs-Delight!</h1>
     </div>
   );
 };
 
-export default SignUp;
+export default Home;
