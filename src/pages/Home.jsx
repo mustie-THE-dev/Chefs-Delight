1ac043107466a33ed50f5dc62b0aa98c3c4256dd
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-28 09:37:38 +0300

    made initial render of my two components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,2 +1,15 @@
 import Veggies from "../components/Veggies";
 import Exciting from "../components/Exciting";
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <Exciting />
+        <Veggies />
+    </div>
+  )
+}
+
+export default Home
