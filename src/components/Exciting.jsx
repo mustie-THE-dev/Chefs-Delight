d1283d6496419a753e124cd35301ef0604856dcc
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-28 11:15:04 +0300

    made changes to my fetch function

--- src/components/Exciting.jsx
+++ src/components/Exciting.jsx
@@ -10,7 +10,7 @@ function Exciting() {
  
  // fetching data from a public API
 const getExciting = async () => {
-  const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.MY_API_KEY}&number=12`);
+  const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.ENV.MY_API_KEY}&number=12`);
   const data = await api.json();
   console.log(data);
 }
