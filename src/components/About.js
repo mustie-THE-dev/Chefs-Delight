f6e757f7b686dd051e3ece7ca26d175f8720a842
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-29 00:17:30 +0300

    styled the about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function About() {
-  return <h1>This is my about component!</h1>;
+  return <p
+  style={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '60vh',
+    fontSize:'80px'
+    
+  }}
+  >Chef Delights is here to help you cook delicious meals with less stress and more joy. We offer recipes and cooking advice for home cooks, by home cooks. Helping create “kitchen wins” is what we are all about. </p>;
 }
 
 export default About;
