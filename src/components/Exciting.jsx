263dc7f4721f123e207bbcca506004fdd1b21172
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-28 13:04:09 +0300

    created a db.json file with data

--- src/components/Exciting.jsx
+++ src/components/Exciting.jsx
@@ -1,25 +1,30 @@
-import { useEffect } from "react"
-
+import React from "react";
+import { useEffect, useState } from "react";
 
 function Exciting() {
+  const [Exciting, setExciting] = useState([]);
 
- useEffect(() => {
-   
-   getExciting();
- },[]); 
- 
- // fetching data from a public API
-const getExciting = async () => {
-  const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.ENV.MY_API_KEY}&number=12`);
-  const data = await api.json();
-  console.log(data);
+  useEffect(() => {
+    getExciting();
+  }, []);
+
+  // fetching data from a public API
+  const APP_ID = "112046c8";
+  const APP_KEY = "1f296129c2a0d3314528cee815e5d631";
+ // const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getExciting = async () => {
+    const api = await fetch(`
+ https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+
+    const data = await api.json();
+    setExciting(data.recipes)
+  };
+
+  return <div>
+
+    {Exciting.map}
+  </div>;
 }
 
-
-    return (
-      <div>Exciting</div>
-    )
-  }
-
-
-export default Exciting
+export default Exciting;
