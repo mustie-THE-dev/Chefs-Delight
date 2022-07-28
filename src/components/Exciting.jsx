180fa39a00c88f600f079324c6ede1dc8dd360de
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-28 10:47:12 +0300

    created an env file for api key

--- src/components/Exciting.jsx
+++ src/components/Exciting.jsx
@@ -10,8 +10,9 @@ function Exciting() {
  
  // fetching data from a public API
 const getExciting = async () => {
-  const api= await fetch(`https://api.spoonacular.com/recipes/random?apikey=${process.env.MY_API_KEY}&number=12`);
+  const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.MY_API_KEY}&number=12`);
   const data = await api.json();
+  console.log(data);
 }
 
 
