024b3598d46f1dc64d50b7a9df9b0efb3cbc59d0
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-28 10:08:13 +0300

    created function to fetch data from a public API

--- src/components/Exciting.jsx
+++ src/components/Exciting.jsx
@@ -1,4 +1,17 @@
 function Exciting() {
+  
+ useEffect(() => {
+   
+   getExciting();
+ },[]); 
+ 
+ // fetching data from a public API
+const getExciting = async () => {
+  const api= await fetch(`https://api.edamam.com/api/recipes/v2?apikey=${process.env.MY_API_KEY}&number=12`);
+  const data = await api.json();
+}
+
+
     return (
       <div>Exciting</div>
     )
--- src/components/Veggies.jsx
+++ src/components/Veggies.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Veggies() {
+  
   return (
     <div>Veggies</div>
   )
